48e0e25c34717712ccab3248021910d7c4a9dce6
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-07-22 11:22:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example_bucketoafvubdeoufbao" {
-  bucket = "my-example-bucketoafvubdeoufbao"
+resource "aws_s3_bucket" "example_bucket1234" {
+  bucket = "my-example-bucket123er5"
   acl    = "private"
 }
 
 resource "aws_s3_bucket_object" "example_object" {
-  bucket = aws_s3_bucket.example_bucket.bucket
+  bucket = aws_s3_bucket.example_bucket.id
   key    = "example-object"
   source = "path/to/local/file"
   etag   = filemd5("path/to/local/file")
@@ -21,9 +21,9 @@ resource "aws_s3_bucket_policy" "example_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Deny"
-        Action = "s3:DeleteObject"
-        Resource = "${aws_s3_bucket.example_bucket.arn}/*"
+        Effect = "Deny",
+        Action = "s3:DeleteObject",
+        Resource = "${aws_s3_bucket.example_bucket.arn}/*",
         Principal = "*"
       }
     ]
